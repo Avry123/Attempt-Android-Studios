481600ad3865bf31cedecff90eec381f1db7095a
Neel Chalke <chalkeavry12@gmail.com>
2023-03-17 01:28:48 +0530

    Life Cycle methods added along with some minor changes

--- app/src/main/java/com/example/maps/MainActivity2.java
+++ app/src/main/java/com/example/maps/MainActivity2.java
@@ -5,21 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
     EditText email, password;
+
+    Button goBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         email = findViewById(R.id.email1);
         password = findViewById(R.id.password1);
+        goBack = findViewById(R.id.goBack);
         Intent intent2 = getIntent();
         Bundle stuff = intent2.getExtras();
         String emailString = stuff.getString("email2");
         String passwordString = stuff.getString("password2");
        email.setText(emailString);
        password.setText(passwordString);
+       Intent intent1 = new Intent(this, MainActivity.class);
+       goBack.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               startActivity(intent1);
+           }
+       });
     }
 }
--- app/src/main/java/com/example/maps/MainActivity4.java
+++ app/src/main/java/com/example/maps/MainActivity4.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -23,6 +24,42 @@ public class MainActivity4 extends AppCompatActivity {
                 startActivity(intent1);
             }
         });
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("onRestart()", "restart method is triggerd");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("onStop()", "stop method is triggerd");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("onStart()", "start method is triggered");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("onResume()", "resume method is triggerd");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("onPause()", "pause method is triggered");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("onDestroy()", "destroy method is triggerd");
     }
 
 
